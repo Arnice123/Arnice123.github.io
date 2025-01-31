ceeea135ad5231049cb65fa2bbc7c246c686c7d8
Arya Akhavein <aryaakhavein92@gmail.com>
2025-01-30 16:08:09 -0800

    Added boxes to redirect to socials (github, linkedin, email)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import ProfilePhoto from './ProfilePhoto';
+import Boxes from './Boxes';
 
 function App() {
   return (
     <div>
-      <h1>Welcome to My Website</h1>
+      <h1 style={{ color: '#000333', fontSize: '2.5rem', textAlign: 'center' }}>About Me</h1>
       <ProfilePhoto /> {}
+      <Boxes></Boxes>
     </div>
   );
 }
