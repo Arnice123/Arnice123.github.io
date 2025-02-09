e449b99f8a2a849383dadc364022a0f9b38b37aa
Arya Akhavein <aryaakhavein92@gmail.com>
2025-02-09 11:07:36 -0800

    Added Links to 3 of my previous projects

--- src/Pages/PreviousProjects.js
+++ src/Pages/PreviousProjects.js
@@ -16,7 +16,26 @@ const PreviousProject = () => {
         />
       </a>
 
-      
+      <p style={{margin: 20, color: "#FFFFFF"}}>
+        My fighting game: {' '}
+        <a style={{ color: 'pink' }} href="https://play.unity.com/en/games/7a22f993-50c4-4830-95f2-9634187cfcbd/2d-fighting-game" target="_blank" rel="noopener noreferrer">
+          2D Fighting Game
+        </a>
+      </p>
+
+      <p style={{margin: 20, color: "#FFFFFF"}}>
+        My Machine Learning Snake: {' '}
+        <a style={{ color: 'pink' }} href="https://github.com/Arnice123/Machine-Learning-Snake" target="_blank" rel="noopener noreferrer">
+          2D Fighting Game
+        </a>
+      </p>
+
+      <p style={{margin: 20, color: "#FFFFFF"}}>
+        My Calculator: {' '}
+        <a style={{ color: 'pink' }} href="https://github.com/Arnice123/Complex-Calulator" target="_blank" rel="noopener noreferrer">
+          Calculator
+        </a>
+      </p>
     </div>
   );
 };
