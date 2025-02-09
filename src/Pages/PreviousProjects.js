0413e01521453363381db8245b651279d3c009a0
Arya Akhavein <aryaakhavein92@gmail.com>
2025-02-09 12:09:34 -0800

    Added descriptions to each project

--- src/Pages/PreviousProjects.js
+++ src/Pages/PreviousProjects.js
@@ -2,14 +2,16 @@ import React from 'react';
 import '../App.css'
 import '../Button.css';
 
-function ProjectLink({ title, link, name, image_path }) {
+function ProjectLink({ title, link, name, description, image_path }) {
   return (
     <div style={{ margin: 20, color: '#FFFFFF' }}>
       <p>
         {title}:{' '}
         <a style={{ color: 'pink' }} href={link} target="_blank" rel="noopener noreferrer">
           {name}
-        </a>
+        </a> 
+        <br /><br /> 
+        {description}
       </p>
       {/* Display the image on a new line */}
       {image_path && (
@@ -39,18 +41,21 @@ const PreviousProject = () => {
         title="My fighting game"
         link="https://play.unity.com/en/games/7a22f993-50c4-4830-95f2-9634187cfcbd/2d-fighting-game"
         name="2D Fighting Game"
+        description="This is a game that allows for either 2 person combat, or one person against AI combat. It was developed over the course of around 7 months in Unity."
         image_path="/images/Fighting-Game.png"
       />
       <ProjectLink
         title="My Machine Learning Snake"
         link="https://github.com/Arnice123/Machine-Learning-Snake"
         name="Machine Learning Snake"
+        description="This is a machine learning model that uses python and Deep-Q learning to beat the classic game Snake. It was developed over the course of a few days after a few weeks of learning python."
         image_path="/images/Machine-Learning-Snake.png"
       />
       <ProjectLink
         title="My Calculator"
         link="https://github.com/Arnice123/Complex-Calulator"
         name="Calculator"
+        description="This is a calculator that was designed without using the main math funciton in C++, cmath, intended to do complex calculations such as derivatives and integrals, it is still being worked on and actively developed"
         image_path="/images/Calculator.png"
       />
     </div>
