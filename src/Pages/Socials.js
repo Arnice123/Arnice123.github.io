29ed6f2da05b8852eb9b8b92c55a57a9180e8b6f
Arya Akhavein <aryaakhavein92@gmail.com>
2025-02-09 16:20:28 -0800

    Added Linkedin, github and email options

--- src/Pages/Socials.js
+++ src/Pages/Socials.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaLinkedin, FaGithub, FaTwitter } from 'react-icons/fa'; // Import icons
+import { FaLinkedin, FaGithub, FaTwitter, FaVoicemail, FaEnvelope } from 'react-icons/fa'; // Import icons
 import SocialPreview from '../SocialPreview';
 import '../App.css'
 
@@ -23,7 +23,32 @@ const Socials = () => {
         }}
       >
         
-        
+        {/* LinkedIn Preview */}
+        <SocialPreview
+          href="https://www.linkedin.com/in/arya-akhavein-8b8029193"
+          icon={<FaLinkedin style={{ fontSize: '28px', marginRight: '15px' }} />}
+          title="LinkedIn"
+          description="Professional Network"
+          color="#0077B5"
+        />
+
+        {/* GitHub Preview */}
+        <SocialPreview
+          href="https://github.com/Arnice123"
+          icon={<FaGithub style={{ fontSize: '28px', marginRight: '15px' }} />}
+          title="GitHub"
+          description="Code & Projects"
+          color="#333333"
+        />
+
+        {/* Email Preview */}
+        <SocialPreview
+          href="mailto:aka310@sfu.ca"
+          icon={<FaEnvelope style={{ fontSize: '28px', marginRight: '15px' }} />}
+          title="Email"
+          description="Direct and Quick Contact"
+          color="#333333"
+        />
 
         
       </div>
